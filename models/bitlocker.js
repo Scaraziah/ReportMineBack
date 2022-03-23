bfd365f31349b5271f3d11c11d92338b9743ccd9
Larry Hatcher <larry.hatcher@hstpathways.com>
2022-03-23 17:29:47 -0500

    Updated string lenght values.

--- models/bitlocker.js
+++ models/bitlocker.js
@@ -21,17 +21,17 @@ const Bitlocker = mongoose.model('Bitlocker',  bitlockerSchema);
 
 function validateBitlocker(bitlocker) {
     const schema =Joi.object({
-        computerName: Joi.string().min(5).max(50).required(),
-        serviceTag: Joi.string().min(5).max(255).required(),
-        currentUser: Joi.string().min(5).max(255),
-        remoteOffice: Joi.string().min(5).max(1024).required(),
+        computerName: Joi.string().min(1).max(150).required(),
+        serviceTag: Joi.string().min(1).max(255).required(),
+        currentUser: Joi.string().min(1).max(255),
+        remoteOffice: Joi.string().min(1).max(1024).required(),
         driveName: Joi.string().min(1).max(1024).required(),
-        driveType: Joi.string().min(5).max(1024).required(),
-        proStatus: Joi.string().min(5).max(1024).required(),
-        encryStatus: Joi.string().min(2).max(1024).required(),
-        lockStatus: Joi.string().min(5).max(1024).required(),
+        driveType: Joi.string().min(1).max(1024).required(),
+        proStatus: Joi.string().min(1).max(1024).required(),
+        encryStatus: Joi.string().min(1).max(1024).required(),
+        lockStatus: Joi.string().min(1).max(1024).required(),
         encryMethod: Joi.string().min(1).max(1024).required(),
-        recKey: Joi.string().min(5).max(1024).required(),
+        recKey: Joi.string().min(1).max(1024).required(),
     });
     return schema.validate(bitlocker);
 }
